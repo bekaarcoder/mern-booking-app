3bc7cd956f9a673f518ec344baa56cf121a5d580
Shashank <s_shashank@outlook.com>
2023-12-23 00:22:27 +0530

    added register route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cors());
 app.get('/api/test', async (req: Request, res: Response) => {
     res.json({ message: 'Hello from Express server!' });
 });
+
+app.use('/api/users', userRoutes);
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
